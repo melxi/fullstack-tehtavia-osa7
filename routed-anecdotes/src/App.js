303b0926ab352ca6a1496abde59e2a621dbae05c
Melxi <magomed.futijev@gmail.com>
2019-07-29 21:47:33 +0300

    7.2: routed anecdotes, step2

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -21,10 +21,21 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  const { author, content, info, votes } = anecdote
+  return (
+    <div>
+      <h1>{content} by {author}</h1>
+      <p>has {votes} votes</p>
+      <p>for more info see <a href={info}>{info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -132,6 +143,7 @@ const App = () => {
       <Router>
         <Menu />
         <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
+        <Route exact path="/anecdotes/:id" render={({ match }) => <Anecdote anecdote={anecdoteById(match.params.id)}/>} />
         <Route exact path="/about" render={() => <About />}/>
         <Route exact path="/create" render={() => <CreateNew addNew={addNew} />} />
       </Router>
